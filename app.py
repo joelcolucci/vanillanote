573fd4c6230df2c023a0daa931b5b0a167437d20
JoelColucci <JoelColucci@Gmail.com>
2015-05-18 07:43:01 -0400

    rolled backed sqlalchemy to earlier v, fixed issues

--- app.py
+++ app.py
@@ -188,6 +188,31 @@ def gconnect():
     return output
 
 
+# User Helper Functions
+
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+
 #DISCONNECT - Revoke a current user's token and reset their login_session
 @app.route('/gdisconnect')
 def gdisconnect():
